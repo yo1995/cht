5f0f07171c9a76cb6f7788a45313871faf7b2445
cht-laptop <cht@bupt.edu.cn>
2016-08-17 00:57:35 +0800

    update

--- js/photo.js
+++ js/photo.js
@@ -1,21 +1,42 @@
-...
-init: function () {
-    var that = this;
-    $.getJSON("/gallery/data.json", function (data) {
-        that.render(that.page, data);
-        that.scroll(data);
-    });
-},
-render: function (page, data) {
-    var begin = (page - 1) * this.offset;
-    var end = page * this.offset;
-    if (begin >= data.length) return;
-    var html, li = "";
-    for (var i = begin; i < end && i < data.length; i++) {
-        li += '<li><div class="img-box">' + '<a class="img-bg" rel="example_group" href="https://raw.githubusercontent.com/yo1995/page-backup/master/photos/' + data[i] + '"></a>' + '<img lazy-src="https://raw.githubusercontent.com/yo1995/page-backup/master/photos/' + data[i] + '" />' + '</li>';
-    }
-    $(".img-box-ul").append(li);
-    $(".img-box-ul").lazyload();
-    $("a[rel=example_group]").fancybox();
-},
-...
+define([], function () {
+	return {
+		page: 1,
+		offset: 20,
+		init: function () {
+			var that = this;
+			$.getJSON("/gallery/data.json", function (data) {
+				that.render(that.page, data);
+				that.scroll(data);
+			});
+		},
+
+		render: function (page, data) {
+			var begin = (page - 1) * this.offset;
+			var end = page * this.offset;
+			if (begin >= data.length) return;
+			var html, li = "";
+			for (var i = begin; i < end && i < data.length; i++) {
+				li += '<li><div class="img-box">' + '<a class="img-bg" rel="example_group" href="https://raw.githubusercontent.com/yo1995/page-backup/master/photos/' + data[i] + '"></a>' + '<img lazy-src="https://raw.githubusercontent.com/yo1995/page-backup/master/photos/' + data[i] + '" />' + '</li>';
+			}
+
+			$(".img-box-ul").append(li);
+			$(".img-box-ul").lazyload();
+			$("a[rel=example_group]").fancybox();
+		},
+
+		scroll: function (data) {
+			var that = this;
+			$(window).scroll(function() {
+				var windowPageYOffset = window.pageYOffset;
+				var windowPageYOffsetAddHeight = windowPageYOffset + window.innerHeight;
+				var sensitivity = 0;
+
+				var offsetTop = $(".instagram").offset().top + $(".instagram").height();
+
+				if (offsetTop >= windowPageYOffset && offsetTop < windowPageYOffsetAddHeight + sensitivity) {
+					that.render(++that.page, data);
+				}
+			})
+		}
+	}
+})
