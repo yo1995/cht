5853451b0048bbf93a526f526dcbbea44921e89f
cht-laptop <cht@bupt.edu.cn>
2016-08-18 19:09:16 +0800

    update

--- js/photo.js
+++ js/photo.js
@@ -2,7 +2,7 @@ define([],
 function() {
     return {
         page: 1,
-        offset: 20,
+        offset: 10, //每次加载图片数
         init: function() {
             var t = this;
             $.getJSON("/cht/gallery/data.json",
@@ -14,11 +14,13 @@ function() {
         render: function(t, e) {
             var o = (t - 1) * this.offset,
             n = t * this.offset;
+			var reg = /\.\w+$/; 
             if (! (o >= e.length)) {
                 for (var a = "",
-                i = o; i < n && i < e.length; i++) a += '<li><div class="img-box"><a class="img-bg" rel="example_group" href="https://raw.githubusercontent.com/yo1995/page-backup/master/photos/' + e[i] + '"></a><img lazy-src="https://raw.githubusercontent.com/yo1995/page-backup/master/photos/' + e[i] + '" /></li>';
-                $(".img-box-ul").append(a),
-                $(".img-box-ul").lazyload(),
+                i = o; i < n && i < e.length; i++) a += '<li><div class="img-box"  id="imgtxt"><a class="img-bg" rel="example_group" href="https://raw.githubusercontent.com/yo1995/page-backup/master/photos/' + e[i] + '"><img src="https://raw.githubusercontent.com/yo1995/page-backup/master/photos/' + e[i] + '"/></a></div><div id="imgtxt"><p>' + e[i].replace(reg,'') + '</p></div></li>';
+                $(".img-box-ul").append(a);
+                $(".img-box-ul").lazyload();
+				changeSize();
                 $("a[rel=example_group]").fancybox()
             }
         },
@@ -34,3 +36,20 @@ function() {
         }
     }
 });
+
+var changeSize = function(){	
+		if($(document).width() <= 600){
+			$(".img-box").css({"width":"auto", "height":"auto"});
+		}else{
+			var height = $(".img-box-ul").height();
+			var size = Math.max(height*0.26, 157);
+			$(".img-box").width(size).height(size);
+			$("ul").css("float:left");
+		}
+	}
+
+var bind = function(){
+	$(window).resize(function(){
+		changeSize();
+	});
+}
